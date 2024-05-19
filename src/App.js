16aebb29bf837faf2363a428dd05ee7844c97213
Phrompong <phrompong.kht@gmail.com>
2024-05-19 10:45:35 +0700

    Add header font and information page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const headers = [
+    "<Home>",
+    "<Skills>",
+    "<Experience>",
+    "<Contact>"
+  ]
+  const nickName = "<CHOPPER>";
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {/* Header */ }
+      <div className='header'>
+        <div className='header-list'>
+          { headers.map((o, index) => (
+            < div > { o }</div>
+          )) }
+        </div>
+      </div>
+
+      {/* Information */ }
+      <div className='information'>
+        <div className='information-first-name'>PHROMPONG</div>
+        <div className='information-first-name'></div>
+        <div className='information-group-last-name-nick-name'>
+          <div className='information-last-name'>KHAGTES</div>
+          <div className='information-nick-name'>{ nickName }</div>
+        </div>
+      </div>
+    </div >
   );
 }
 
