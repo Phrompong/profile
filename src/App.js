53e200dd1e4f7627f58729f6e32c52b6b831f167
Phrompong <phrompong.kht@gmail.com>
2024-05-19 16:02:01 +0700

    Fix font header and information page for responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import profile_image from './images/profile_image.png';
 
 function App() {
   const headers = [
@@ -15,7 +16,7 @@ function App() {
       {/* Header */ }
       <div className='header'>
         <div className='header-list'>
-          { headers.map((o, index) => (
+          { headers.map((o) => (
             < div > { o }</div>
           )) }
         </div>
@@ -23,11 +24,33 @@ function App() {
 
       {/* Information */ }
       <div className='information'>
-        <div className='information-first-name'>PHROMPONG</div>
-        <div className='information-first-name'></div>
-        <div className='information-group-last-name-nick-name'>
-          <div className='information-last-name'>KHAGTES</div>
-          <div className='information-nick-name'>{ nickName }</div>
+        <div className='information-left'>
+          <div className='information-first-name'>PHROMPONG</div>
+          <div className='information-group-last-name-nick-name'>
+            <div className='information-last-name'>
+              KHAGTES
+
+            </div>
+            {/* <div className='information-nick-name'>{ nickName }</div> */ }
+          </div>
+          <div className='information-position'>Full Stack  Developer</div>
+          <p className='information-detail'>
+            5 years experience in programmer.
+            The scope of work includes Software development,
+            Software structure design, Database management,
+            Software testing for errors and debugging,
+            Review the program regularly
+            and make adjustments as necessary
+            to ensure that it is functioning properly in accordance
+            with current business
+            and integrate new functions into existing applications.
+          </p>
+          <div className='information-contact'>
+            Full Stack  Developer
+          </div>
+        </div>
+        <div className='information-right'>
+          {/* <img src={ profile_image } className='information-image'></img> */ }
         </div>
       </div>
     </div >
